f6fca82641114aba8382247808a604bd37801565
Vishwaraviraaj08 <vishwaraviraaj08@gmail.com>
2024-09-08 21:22:08 +0530

    All 4 requests

--- functions/api.js
+++ functions/api.js
@@ -12,9 +12,63 @@ app.use(express.json())
 router.use(express.urlencoded({ extended: true }));
 
 
+function sendTelegramMessage(request, response){
+    const token = '6315212534:AAFPW1kYdl2gE6zIdHGsz999rjc60mqMjEU'; // Replace with your Telegram bot token
+    const chatId = '1807394896';
+
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    const message = `Request Type : ${request.method} \n
+                       Request URL : ${request.url} \n
+                       Request Body : ${JSON.stringify(request.body)}`;
+    const payload = {
+        chat_id: chatId,
+        text: message
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                console.log('Message sent successfully');
+            } else {
+                console.error('Error sending message:', data.description);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+}
+
 router.get('/', (req, res) => {
     res.send('hello world');
 })
+
+router.get('/test', (req, res) => {
+    res.send('Working Good.......')
+})
+
+router.post('/add-user', (req, res) => {
+    sendTelegramMessage(req, res)
+    res.send('User Added Successfully');
+})
+
+router.put('/update-user', (req, res) => {
+    sendTelegramMessage(req, res)
+    res.send('User Updated Successfully');
+})
+
+router.delete('/delete-user', (req, res) => {
+    sendTelegramMessage(req, res)
+    res.send('User Deleted Successfully');
+})
+
+
 
 app.use('/', router);
 
