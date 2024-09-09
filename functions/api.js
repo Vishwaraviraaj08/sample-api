097e68ab466db8a9e27d363e826db85994d56854
Vishwaraviraaj08 <vishwaraviraaj08@gmail.com>
2024-09-09 15:06:45 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -63,7 +63,7 @@ router.put('/update-user', (req, res) => {
 
 router.delete('/delete-user', (req, res) => {
     sendTelegramMessage(req, res)
-    throw new Error("User Not Found");
+    res.send({"status":"200", "message":'User deleted Successfully'})
 })
 
 router.post('/login', (req, res) => {
